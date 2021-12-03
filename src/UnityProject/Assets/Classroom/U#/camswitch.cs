12e229b4db3e0440b4d55af771f9842cd28ca380
hsm <smhan213@gmail.com>
2021-12-03 16:25:54 +0900

    scene and function

---
+++ src/UnityProject/Assets/Classroom/U#/camswitch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+
+public class camswitch : UdonSharpBehaviour
+{
+    public GameObject cam1;
+    public GameObject cam2;
+
+    void Update()
+    {
+        if(Input.GetKey("1")){
+            cam1.SetActive(true);
+            cam2.SetActive(false);
+        }
+        else if(Input.GetKey("2")){
+            cam1.SetActive(false);
+            cam2.SetActive(true);
+        }
+    }
+}
