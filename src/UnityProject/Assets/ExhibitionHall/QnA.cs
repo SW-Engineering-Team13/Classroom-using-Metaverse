13c781719cf980eba860ab9e4793fbf43d3a9e35
jjeongah <j_eah@naver.com>
2021-12-03 14:19:24 +0900

    change inputfield

--- src/UnityProject/Assets/ExhibitionHall/QnA.cs
+++ src/UnityProject/Assets/ExhibitionHall/QnA.cs
@@ -7,11 +7,12 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        /*
         var input = gameObject.GetComponent<InputField>();
         var se= new InputField.SubmitEvent();
         se.AddListener(SubmitName);
         input.onEndEdit = se;
-
+        */
         //or simply use the line below, 
         //input.onEndEdit.AddListener(SubmitName);  // This also works 
     }
