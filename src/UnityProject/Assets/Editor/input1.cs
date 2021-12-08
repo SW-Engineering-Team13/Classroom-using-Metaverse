99a2b660260c9d3b9b47ac2562295e71dce94536
jjeongah <j_eah@naver.com>
2021-12-08 20:42:12 +0900

    input function

---
+++ src/UnityProject/Assets/Editor/input1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//https://naakjii.tistory.com/83
+
+public class input : MonoBehaviour
+{
+
+    public InputField mainInputField1; 
+    // Checks if there is anything entered into the input field. 
+    void LockInput(InputField input) { 
+        if (input.text.Length > 0) { 
+            Debug.Log("Text has been entered"); 
+            } 
+        else if (input.text.Length == 0) { Debug.Log("Main Input Empty"); } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Use onEndEdit 
+        mainInputField.onEndEdit.AddListener(delegate{LockInput(mainInputField1);});
+        
+        //Use onSubmit 
+        mainInputField.onSubmit.AddListener(delegate{LockInput(mainInputField1);});
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ src/UnityProject/Assets/Editor/input2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//https://geukggom.tistory.com/143
+public class input2 : MonoBehaviour
+{
+    public InputField myInputField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myInputField.onValueChange.AddListener(ValueChanged);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         Debug.Log(text + " - 글자 입력 중");
+    }
+}
