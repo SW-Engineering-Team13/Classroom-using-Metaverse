f1e380de4f28a55ed5f525b7717fda359aa9a048
jjeongah <j_eah@naver.com>
2021-12-08 21:13:47 +0900

    add color effect

---
+++ src/UnityProject/Assets/FadeColor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeColor : MonoBehaviour
+{
+    SpriteRenderer sr;
+    public GameObject go;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = go.GetComponent<SpritRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(input.GetKeyDown("r"))
+            sr.material.color = Color.red;
+        if(input.GetKeyDown("g"))
+            sr.material.color = Color.green;
+        if(input.GetKeyDown("b"))
+            sr.material.color = Color.blue;    
+    }
+}
