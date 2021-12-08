2cf1290574bc6721be972caaa7f5e3dcc88686b0
jjeongah <j_eah@naver.com>
2021-12-08 21:28:37 +0900

    add present object

--- src/UnityProject/Assets/FadeColor.cs
+++ src/UnityProject/Assets/FadeColor.cs
@@ -20,6 +20,16 @@ public class FadeColor : MonoBehaviour
         if(input.GetKeyDown("g"))
             sr.material.color = Color.green;
         if(input.GetKeyDown("b"))
-            sr.material.color = Color.blue;    
+            sr.material.color = Color.blue;   
+        if(input.GetKeyDown("m"))
+            sr.material.color = Color.magenta;
+        if(input.GetKeyDown("y"))
+            sr.material.color = Color.yellow;
+        if(input.GetKeyDown("c"))
+            sr.material.color = Color.cyan;
+        if(input.GetKeyDown("k"))
+            sr.material.color = Color.black;   
+        if(input.GetKeyDown("e"))
+            sr.material.color = Color.gray;                   
     }
 }
