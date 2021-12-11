f6a19aa2b36ea6959389e06c9b03324a06ca5510
jjeongah <j_eah@naver.com>
2021-12-11 09:51:45 +0900

    add cs files

---
+++ src/UnityProject/Assets/Editor/FadeColor2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeColor2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public SpriteRenderer sr;
+    public GameObject go;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = go.GetComponent<SpritRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("r"))
+            sr.material.color = Color.red;
+        if(Input.GetKeyDown("g"))
+            sr.material.color = Color.green;
+        if(Input.GetKeyDown("b"))
+            sr.material.color = Color.blue;   
+        if(Input.GetKeyDown("m"))
+            sr.material.color = Color.magenta;
+        if(Input.GetKeyDown("y"))
+            sr.material.color = Color.yellow;
+        if(Input.GetKeyDown("c"))
+            sr.material.color = Color.cyan;
+        if(Input.GetKeyDown("k"))
+            sr.material.color = Color.black;   
+        if(Input.GetKeyDown("e"))
+            sr.material.color = Color.gray;                   
+    }
+}
