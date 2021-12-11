310a903605d78d7131e9da37d430d6dcbb34968f
seyrinn <79247355+seyrinn@users.noreply.github.com>
2021-12-12 00:21:00 +0900

    fix teleport system

--- src/UnityProject/Assets/Classroom/U#/AutoTeleportSystem.cs
+++ src/UnityProject/Assets/Classroom/U#/AutoTeleportSystem.cs
@@ -1,4 +1,4 @@
-﻿
+
 using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
@@ -9,6 +9,7 @@ using System;
 public class AutoTeleportSystem : UdonSharpBehaviour
 {
     public ClassroomData classroomData;
+    public GameObject[] gameObjectsArr;
 
     Boolean flag = true; //update tickrate controll
     int flagminute = -1;
@@ -23,7 +24,6 @@ public class AutoTeleportSystem : UdonSharpBehaviour
         if (hour == classroomData.classStartTime.AddMinutes(-5).Hour && minute == classroomData.classStartTime.AddMinutes(-5).Minute && flag)
         {
             teleport();
-            Debug.Log("teleport");
         }
         flag = false;
 
@@ -37,8 +37,16 @@ public class AutoTeleportSystem : UdonSharpBehaviour
     }
 
     public void teleport(){
+        float x1 = gameObjectsArr[0].transform.localPosition.x;
+        float x2 = gameObjectsArr[0].transform.localPosition.x;
+
+        float z1 = gameObjectsArr[0].transform.localPosition.z;
+        float z2 = gameObjectsArr[0].transform.localPosition.z;
+
         Vector3 pos = Networking.LocalPlayer.GetPosition();
-        if (pos.x > -10 && pos.x < 10 && pos.z > - 15 && pos.z < 15){
+
+        /* 이미 강의실 안 */
+        if (pos.x > x1 && pos.x < x2 && pos.z > - z2 && pos.z < z1){
             return;
         }else{
             Debug.Log("Teleported!");
