5a65c20b4127353dba8b1183f398869d64721058
jjeongah <j_eah@naver.com>
2021-12-12 00:53:21 +0900

    add udon file(error exist)

--- src/UnityProject/Assets/Editor/QnA.cs
+++ src/UnityProject/Assets/Editor/QnA.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿//from QnA2.cs
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
---
+++ src/UnityProject/Assets/Editor/input4.cs
@@ -0,0 +1,23 @@
+﻿//from input2.cs
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class input4 : UdonSharpBehaviour
+{
+   /*
+    public InputField myInputField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myInputField.onValueChange.AddListener(ValueChanged);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         Debug.Log(text + " - 글자 입력 중");
+    }
+    */
+}
