356caffcb7c3febe8ec4531176b359f5384b5a1e
jjeongah <j_eah@naver.com>
2021-12-11 10:39:56 +0900

    add color change effect

--- src/UnityProject/Assets/FadeColor.cs
+++ src/UnityProject/Assets/FadeColor.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FadeColor2 : MonoBehaviour
+public class FadeColor : MonoBehaviour
 {
     // Start is called before the first frame update
     public SpriteRenderer sr;
