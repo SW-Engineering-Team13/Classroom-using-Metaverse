3cc80656939c244f7516e4f9feeac6dcb8e18b73
Lucy <64342804+Na-Hyeon-Oh@users.noreply.github.com>
2021-12-12 10:10:52 +0900

    set descriptor in elevator

---
+++ src/UnityProject/Assets/library/spawnPoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnPoint : MonoBehaviour
+{
+    public GameObject prefabToSpawn;
+    public float repeateInterval;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(repeateInterval > 0){
+            InvokeRepeating("SpawnObject", 0.0f, repeateInterval);
+        }
+        
+    }
+
+    public GameObject SpawnObject(){
+        if(prefabToSpawn != null){
+            return Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
+        }
+
+        return null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
