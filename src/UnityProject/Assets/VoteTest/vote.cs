143e1ae0f65b67d4bb1a9fc279b4ebce9170cff1
Ng Zhi Wei <90780072+NgZhiWei95@users.noreply.github.com>
2021-12-12 14:45:53 +0900

    vote worked

---
+++ src/UnityProject/Assets/VoteTest/vote.cs
@@ -0,0 +1,22 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace votetest
+{
+    public class vote : UdonSharpBehaviour
+    {
+
+        public GameObject Vote;
+        public int counter = 0;
+
+        public override void Interact()
+        {
+            counter++;
+            Vote.GetComponent<Text>().text = counter.ToString();
+        }
+    }
+}
