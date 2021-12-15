d82a2ab7d08fc9c86d6d996ffeb4d12936d2630b
Ng Zhi Wei <90780072+NgZhiWei95@users.noreply.github.com>
2021-12-15 18:52:03 +0900

    updated

---
+++ src/UnityProject/Assets/vote5.cs
@@ -0,0 +1,33 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+
+public class vote5 : UdonSharpBehaviour
+{
+    [UdonSynced, FieldChangeCallback(nameof(SyncedNumber))] private int _syncedNumber = 0;
+    public GameObject object5;
+
+    public int SyncedNumber
+    {
+        set
+        {
+            Debug.Log("toggling the object...");
+            _syncedNumber = value;
+            object5.GetComponent<Text>().text = _syncedNumber.ToString();
+        }
+        get => _syncedNumber;
+    }
+
+    public override void Interact()
+    {
+        Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
+        SyncedNumber++;
+        RequestSerialization();
+    }
+}
+
