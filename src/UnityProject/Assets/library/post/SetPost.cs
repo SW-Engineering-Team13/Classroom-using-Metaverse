dd902812b036755b8bb269709af14a40d1a795df
skkusohee <a41152271@gmail.com>
2021-12-15 15:59:50 +0900

    setpost.cs 수정

--- src/UnityProject/Assets/library/post/SetPost.cs
+++ src/UnityProject/Assets/library/post/SetPost.cs
@@ -32,7 +32,7 @@ public class SetPost : UdonSharpBehaviour
     {
         showposter = null;
         string which = inputField.text;
-        string where = "/post/Prefabs/SceneDescriptionBlock/Canvas/Panel/Poster";
+        string where = "/Library/post/Prefabs/SceneDescriptionBlock/Canvas/Panel/Poster";
         string name = inputField.name;
         if(name == "pathInputField1")
         {
